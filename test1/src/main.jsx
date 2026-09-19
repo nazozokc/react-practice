35d55666128760f66186255ddea25ae34bf5a215
nazozokc <nazozokc@users.noreply.github.com>
2026-09-19 21:18:40 +0900

    a

--- test1/src/main.jsx
+++ test1/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Calendar } from "@typescript-calendar-lib/react";
-import "@typescript-calendar-lib/react/calendar.css";
+
 import "./index.css";
 import App from "./App.jsx";
 
