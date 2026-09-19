25f0a8f1892a4e537becb21a3241fe554f367a0c
nazozokc <nazozokc@users.noreply.github.com>
2026-09-19 20:55:18 +0900

    a

--- test1/src/main.jsx
+++ test1/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Calendar } from "@typescript-calendar-lib/react";
+import "@typescript-calendar-lib/react/calendar.css";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Calendar year={2026} month={9} colorScheme="ocean" theme="modern" />
   </StrictMode>,
-)
+);
